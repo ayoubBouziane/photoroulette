cbb23fc56eb5db45522507801e6b49c49102b9aa
sergiomarquesmoura <sergiomarquesmoura@gmail.com>
2014-06-30 22:22:19 +0100

    a funcionar o facebook, sem deprecated libraries

--- photoroulette/src/main/java/com/photoroulette/MainActivity.java
+++ photoroulette/src/main/java/com/photoroulette/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends Activity {
 
                 if (session.isOpened()) {
                     Log.i(TAG,"Access Token"+ session.getAccessToken());
-                    Request.executeMeRequestAsync(session,
+                    Request.newMeRequest(session,
                             new Request.GraphUserCallback() {
                                 @Override
                                 public void onCompleted(GraphUser user,Response response) {
@@ -62,7 +62,7 @@ public class MainActivity extends Activity {
 
                                     }
                                 }
-                            });
+                            }).executeAsync();
                 }
 
             }
